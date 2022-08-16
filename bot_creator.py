3f720cd4cfe47bf6f7b4278a8897165e052b3a0f
Dmytro <103268527+DiAn445@users.noreply.github.com>
2022-08-16 11:54:43 +0300

    took off the token

--- bot_creator.py
+++ bot_creator.py
@@ -3,5 +3,5 @@ from aiogram.dispatcher import Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 storage = MemoryStorage()
-bot = Bot(token='5539927184:AAHgUjfxubZBti0RhHG4k6rMbD1aCUuUTLM', parse_mode=types.ParseMode.HTML)
+bot = Bot(token='', parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(bot, storage=storage)
